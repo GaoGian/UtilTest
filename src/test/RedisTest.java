db91505359d4ad0514a3902ef33b6d20786cdcda
GaoGian <447647833@qq.com>
2018-12-05 10:59:35 +0800

    更新redis连接

--- src/test/RedisTest.java
+++ src/test/RedisTest.java
@@ -8,12 +8,13 @@ public class RedisTest {
 	public static void main(String[] args) {
 		JedisPoolRedisClientImpl redisClient = new JedisPoolRedisClientImpl();
 		GenericObjectPoolConfig poolConfig = new JedisPoolConfig();
-		redisClient.setHost("192.168.2.23");
-		redisClient.setPassword("");
+		redisClient.setHost("dev-redis.tingyun.com");
+		redisClient.setPassword("nbs!@#123");
 		redisClient.setPoolConfig(poolConfig);
 		redisClient.init();
 		
-		String redisVal = redisClient.get("SERVER_LIMIT_AGREEMENT#1760");
+//		String redisVal = redisClient.get("SERVER_LIMIT_AGREEMENT#1760");
+		redisClient.set("expire_test", "expire_test", 600);
 		System.out.println("------------------------");
 
 	}
